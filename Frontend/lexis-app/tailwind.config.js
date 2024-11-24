6c9d59fff175cd535d426e54f1e93aa11d0e9c1c
Lean Kurt <leankurtescano@gmail.com>
2024-11-24 17:55:08 +0800

    Fix commit

--- Frontend/lexis-app/tailwind.config.js
+++ Frontend/lexis-app/tailwind.config.js
@@ -1,12 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js, ts, jsx, tsx}"
+    './index.html', // If you use plain HTML files
+    './src/**/*.{js,ts,jsx,tsx}', // Include all JS, TS, JSX, and TSX files in the src folder
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
