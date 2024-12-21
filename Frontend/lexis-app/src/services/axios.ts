0860983f3968f4ce188e0a3128525c109a0a7e10
Gio-Majadas <sensei.makima55@gmail.com>
2024-12-21 12:00:58 +0800

    Forgot password and new password ui implemented, ready to connect the back end functionality

--- Frontend/lexis-app/src/services/axios.ts
+++ Frontend/lexis-app/src/services/axios.ts
@@ -1,27 +1,35 @@
 import axios from 'axios';
 
+
+
+
 export const sendPrompt = async(url:string, input: string) => {
+
     const accessToken = localStorage.getItem("access_token");
     const response = await axios.post(`${url}/prompt/`,{
         input: input
     }, {
         headers : {
+
             'Authorization': `Bearer ${accessToken}`,
         }
     })
-    return response
-}
 
+    return response
+
+}
 export const logOut  = async(url:string) => {
     const accessToken = localStorage.getItem("access_token");
     const response = await axios.post(`${url}/logout/`,{}, {
         headers: {
+
             'Authorization': `Bearer ${accessToken}`,
         }
     })
+
+
     return response
 }
-
 export const sendEmailOtp = async(email :string, url:string) =>  {
     const response = await axios.post(`${url}/email-otp/`, {
         email:email,
@@ -33,22 +41,22 @@ export const sendEmailOtp = async(email :string, url:string) =>  {
 }
 
 export const registerUser = async(otpCode :string,url:string) => {
-    const email = sessionStorage.getItem("email");
-    const username = sessionStorage.getItem("username");
-    const password = sessionStorage.getItem("password");
-    const response = await axios.post(`${url}/register/`,{
-        email:email,
-        username: username,
-        password: password,
-        otpCode : otpCode
-    }, {
+   const email = sessionStorage.getItem("email");
+   const username = sessionStorage.getItem("username");
+   const password = sessionStorage.getItem("password");
+   const response = await axios.post(`${url}/register/`,{
+     email:email,
+     username: username,
+     password: password,
+     otpCode : otpCode
+   }, {
     headers : {
         "Content-Type": "application/json"
     }
-    })
-    return response
-}
+   })
 
+   return response
+}
 export const handleSignUp  = async(username:string, email:string, password:string, confirm:string, url:string) => {
     const response = await axios.post(`${url}/signup/`, {
         username: username,
@@ -60,6 +68,7 @@ export const handleSignUp  = async(username:string, email:string, password:strin
             'Content-Type': 'application/json'
         }
     })
+
     return response
 }
 
@@ -69,14 +78,14 @@ export const handleLogin =  async(email:string , password: string, url: string) 
         password : password
         }, {
             headers:{
-                "Content-Type": "application/json"
+                 "Content-Type": "application/json"
             }       
         }
     )
+
     return response
 }
 
-// Implement the handle new password here, connect the new password back end functionality here
 export const handleNewPassword = async (newPassword: string, apiUrl: string) => {
     const accessToken = localStorage.getItem("access_token");
     const response = await axios.post(`${apiUrl}/reset-password/confirm`, {
