a83c9dbb70148e22d954488e332e42889d9e141a
GioMjds <giomjds@gmail.com>
2025-02-10 22:06:54 +0800

    fix: chatbot UI appearance to sidebar toggle

--- Frontend/lexis-app/src/pages/ChatBot.tsx
+++ Frontend/lexis-app/src/pages/ChatBot.tsx
@@ -16,7 +16,7 @@ const ChatBot: FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const [promptStyle, setPromptStyle] = useState<string>("");
-  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false); // Renamed from isDrawerOpen and useState(false) by default
   const apiUrl = import.meta.env.VITE_API_URL2;
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value);
@@ -28,11 +28,12 @@ const ChatBot: FC = () => {
 
   const handleUpdate = async (id: number) => {
     const messageIndex = messages.findIndex(msg => msg.id === id);
+    const updatedMessages = [...messages];
+
     if (messageIndex === -1) return;
-    
+
     setIsLoading(true);
 
-    const updatedMessages = [...messages];
     updatedMessages[messageIndex].text = editText;
 
     updatedMessages.splice(messageIndex + 1);
@@ -93,7 +94,7 @@ const ChatBot: FC = () => {
           setPromptError(data.error);
           break;
         default:
-          alert("Lexscribe is under maintenance. Please try again later.")
+          alert("Lexcribe is under maintenance. Please try again later.")
       }
     } finally {
       setIsLoading(false);
@@ -106,6 +107,7 @@ const ChatBot: FC = () => {
 
   const handleDrawerBtnClick = (action: string) => {
     switch (action) {
+      // To be modified in DrawerButtons.ts
       case 'newChat':
         console.log("new chat");
         break;
@@ -115,183 +117,189 @@ const ChatBot: FC = () => {
       default:
         break;
     }
-    setIsDrawerOpen(false);
+    setIsSidebarOpen(false);
   };
 
-  const toggleDrawer = () => {
-    setIsDrawerOpen(prev => !prev);
-  };
+  const toggleSidebar = () => setIsSidebarOpen(prev => !prev);
 
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
 
   return (
-    <section className="bg-spotlight min-h-screen flex flex-col items-center relative">
-      <AnimatePresence>
-        <button
-          onClick={toggleDrawer}
-          className={`absolute left-0 top-1/2 p-4 bg-black bg-opacity-0 text-white rounded transition-all duration-300 ${isDrawerOpen ? 'opacity-0' : 'opacity-100'}`}
-        >
-          <i className="fas fa-door-open"></i>
-        </button>
+    <AnimatePresence>
+      <section className="bg-spotlight min-h-screen flex flex-col items-center relative">
+          <button
+            onClick={toggleSidebar}
+            className={`absolute left-4 top-4 p-2 bg-black bg-opacity-0 text-white rounded-full transition-all duration-300 hover:bg-opacity-50 focus:outline-none`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
 
         <Sidebar
-          isOpen={isDrawerOpen}
-          onClose={() => setIsDrawerOpen(false)}
+          isOpen={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
           drawerBtnClick={handleDrawerBtnClick}
         />
-      </AnimatePresence>
 
-      {!hasStarted ? (
+        {!hasStarted ? (
+          <motion.div
+            className={`h-[calc(100vh-200px)] flex flex-col items-center justify-center transition-all duration-300 ease-in-out ${isSidebarOpen ? 'ml-52' : ''}`}
+            initial="hidden"
+            animate="visible"
+            variants={fadeVariants}
+          >
+            <h1 className="text-3xl text-center font-bold tracking-tight leading-none text-light-high md:text-5xl lg:text-6xl dark:text-white">Ask <span className="text-purple-600 text-opacity-60">Lexcribe</span> about Law</h1>
+            <p className="mt-4 text-center text-lg font-normal text-light-medium">World's First AI Chatbot for Law Students</p>
+          </motion.div>
+        ) : (
+          <div className={`flex flex-col w-full ${isSidebarOpen ? 'max-w-screen-lg' : 'max-w-screen-xl'} mt-8 space-y-4 p-3 overflow-y-auto max-h-[calc(100vh-200px)] scrollbar-none  transition-all duration-300 ease-in-out ${isSidebarOpen ? 'ml-52' : ''}`}>
+            {messages.map((message) => (
+              <motion.div
+                key={message.id}
+                className={`flex items-center gap-2 ${message.type === 'user' ? 'justify-end' : 'justify-start'}`}
+                initial="hidden"
+                animate="visible"
+              >
+                {message.type === 'user' && (
+                  <button
+                    onClick={() => handleEdit(message.id, message.text)}
+                    className="text-gray-400 hover:bg-gray-500/50 p-2 rounded-full w-10 h-10 transition-colors"
+                  >
+                    <i className="fas fa-edit"></i>
+                  </button>
+                )}
+
+                <div className={`p-3 rounded-lg text-base ${message.type === 'user'
+                  ? 'bg-sky-700/15 font-medium text-white'
+                  : 'bg-sky-700/15 font-medium text-white w-4/6'
+                  }`} ref={messagesEndRef}
+                >
+                  {editingId === message.id ? (
+                    <Suspense fallback={<MiniSkeleton />}>
+                      <div className="flex items-center gap-2">
+                        <input
+                          type="text"
+                          value={editText}
+                          onChange={(e) => setEditText(e.target.value)}
+                          className="w-full px-2 py-1 text-gray-100 bg-transparent border-none rounded focus:outline-none"
+                        />
+                        <button
+                          onClick={() => handleUpdate(message.id)}
+                          className="text-white hover:bg-gray-600/20 p-1 rounded transition-colors duration-200"
+                        >
+                          <i className="fas fa-check"></i>
+                        </button>
+                        <button
+                          onClick={() => setEditingId(null)}
+                          className="text-white hover:bg-gray-600/20 p-1 rounded transition-colors duration-200"
+                        >
+                          <i className="fas fa-times"></i>
+                        </button>
+                      </div>
+                    </Suspense>
+                  ) : (
+                    <motion.span
+                      initial="hidden"
+                      animate="visible"
+                      variants={textTypography}
+                    >
+                      {message.type === 'user' ? (
+                        <motion.span
+                          initial="hidden"
+                          animate="visible"
+                          variants={textTypography}
+                        >
+                          {message.text}
+                        </motion.span>
+                      ) : (
+                        <motion.span
+                          initial="hidden"
+                          animate="visible"
+                          variants={textTypography}
+                        >
+                          {message.text.split('').map((char, index) => (
+                            <motion.span
+                              key={index}
+                              initial={{ opacity: 0 }}
+                              animate={{ opacity: 1 }}
+                              transition={{ duration: 0.001, delay: index * 0.01 }}
+                            >
+                              {char}
+                            </motion.span>
+                          ))}
+                        </motion.span>
+                      )}
+                    </motion.span>
+                  )}
+                </div>
+              </motion.div>
+            ))}
+            {isLoading && (
+              <Suspense fallback={null}>
+                <MiniSkeleton />
+              </Suspense>
+            )}
+          </div>
+        )}
+
         <motion.div
-          className="h-[calc(100vh-200px)] flex flex-col items-center justify-center"
+          className={`flex flex-col justify-center items-center w-full max-w-4xl mb-4 p-2 dark:bg-gray-900 fixed bottom-0 transition-all duration-300 ease-in-out ${isSidebarOpen ? 'ml-52' : ''}`}
           initial="hidden"
           animate="visible"
           variants={fadeVariants}
         >
-          <h1 className="text-3xl text-center font-bold tracking-tight leading-none text-light-high md:text-5xl lg:text-6xl dark:text-white">Ask <span className="text-purple-600 text-opacity-60">Lexcribe</span> about Law</h1>
-          <p className="mt-4 text-center text-lg font-normal text-light-medium">World's First AI Chatbot for Law Students</p>
-        </motion.div>
-      ) : (
-        <div className="flex flex-col w-full max-w-screen-xl mt-8 space-y-4 p-3 overflow-y-auto max-h-[calc(100vh-200px)] scrollbar-none">
-          {messages.map((message) => (
-            <motion.div
-              key={message.id}
-              className={`flex items-center gap-2 ${message.type === 'user' ? 'justify-end' : 'justify-start'}`}
-              initial="hidden"
-              animate="visible"
+          <div className="flex flex-col justify-start items-start w-full py-3">
+            <select
+              value={promptStyle}
+              onChange={(e) => setPromptStyle(e.target.value)}
+              className="w-48 bg-white text-white text-sm bg-opacity-5 border-none rounded-xl sm:mr-2 outline-none active:border-none focus:border-none"
             >
-              {message.type === 'user' && (
-                <button
-                  onClick={() => handleEdit(message.id, message.text)}
-                  className="text-gray-400 hover:bg-gray-500/50 p-2 rounded-full w-10 h-10 transition-colors"
-                >
-                  <i className="fas fa-edit"></i>
-                </button>
-              )}
+              <option value='default' className='bg-gray-900/90'>Foundation</option>
+              <option value="eli5" className='bg-gray-900/90'>Explanation</option>
+              <option value="highSchool" className='bg-gray-900/90'>Application</option>
+              <option value="college" className='bg-gray-900/90'>Jurisprudence</option>
+              <option value="expert" className='bg-gray-900/90'>Citation</option>
+            </select>
+          </div>
+          <form onSubmit={handleSend} className="flex flex-row justify-center items-center w-full p-1">
+            <input
+              type="text"
+              value={input}
+              onChange={handleInputChange}
+              className="flex-grow p-4 text-base sm:mb-0 h-14 rounded-full caret-purple-600 text-gray-900"
+              placeholder="Ask Lexcribe"
+            />
+            <button
+              type="submit"
+              className="p-3 ml-2 text-gray-900 text-2xl bg-gradient-to-br from-teal to-sky-600 border border-gray-500 rounded-full w-14 h-14 transition duration-200"
+            >
+              <i className="fas fa-paper-plane"></i>
+            </button>
+          </form>
+          <p className="text-center mt-1 text-xs text-gray-400/70">Lexcribe may occasionally provide inaccurate information. Please verify any legal advice with authoritative sources.</p>
+        </motion.div>
 
-              <div className={`p-3 rounded-lg text-base ${message.type === 'user'
-                ? 'bg-sky-700/15 font-medium text-white'
-                : 'bg-sky-700/15 font-medium text-white w-4/6'
-                }`} ref={messagesEndRef}
-              >
-                {editingId === message.id ? (
-                  <Suspense fallback={<MiniSkeleton />}>
-                    <div className="flex items-center gap-2">
-                      <input
-                        type="text"
-                        value={editText}
-                        onChange={(e) => setEditText(e.target.value)}
-                        className="w-full px-2 py-1 text-gray-100 bg-transparent border-none rounded focus:outline-none"
-                      />
-                      <button
-                        onClick={() => handleUpdate(message.id)}
-                        className="text-white hover:bg-gray-600/20 p-1 rounded transition-colors duration-200"
-                      >
-                        <i className="fas fa-check"></i>
-                      </button>
-                      <button
-                        onClick={() => setEditingId(null)}
-                        className="text-white hover:bg-gray-600/20 p-1 rounded transition-colors duration-200"
-                      >
-                        <i className="fas fa-times"></i>
-                      </button>
-                    </div>
-                  </Suspense>
-                ) : (
-                  <motion.span
-                    initial="hidden"
-                    animate="visible"
-                    variants={textTypography}
-                  >
-                    {message.type === 'user' ? (
-                      <motion.span
-                        initial="hidden"
-                        animate="visible"
-                        variants={textTypography}
-                      >
-                        {message.text}
-                      </motion.span>
-                    ) : (
-                      <motion.span
-                        initial="hidden"
-                        animate="visible"
-                        variants={textTypography}
-                      >
-                        {message.text.split('').map((char, index) => (
-                          <motion.span
-                            key={index}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            transition={{ duration: 0.001, delay: index * 0.01 }}
-                          >
-                            {char}
-                          </motion.span>
-                        ))}
-                      </motion.span>
-                    )}
-                  </motion.span>
-                )}
-              </div>
-            </motion.div>
-          ))}
-          {isLoading && (
-            <Suspense fallback={null}>
-              <MiniSkeleton />
-            </Suspense>
-          )}
-        </div>
-      )}
-
-      <motion.div
-        className="flex flex-col justify-center items-center w-full max-w-4xl mb-4 p-2 dark:bg-gray-900 fixed bottom-0"
-        initial="hidden"
-        animate="visible"
-        variants={fadeVariants}
-      >
-        <div className="flex flex-col justify-start items-start w-full py-3">
-          <select
-            value={promptStyle}
-            onChange={(e) => setPromptStyle(e.target.value)}
-            className="w-48 bg-white text-white text-sm bg-opacity-5 border-none rounded-xl sm:mr-2 outline-none active:border-none focus:border-none"
+        {promptError && (
+          <motion.p
+            className="text-red-500 font-bold text-lg"
+            initial='hidden'
+            animate='visible'
+            variants={fadeVariants}
           >
-            <option value='default' className='bg-gray-900/90'>Foundation</option>
-            <option value="eli5" className='bg-gray-900/90'>Explanation</option>
-            <option value="highSchool" className='bg-gray-900/90'>Application</option>
-            <option value="college" className='bg-gray-900/90'>Jurisprudence</option>
-            <option value="expert" className='bg-gray-900/90'>Citation</option>
-          </select>
-        </div>
-        <form onSubmit={handleSend} className="flex flex-row justify-center items-center w-full p-1">
-          <input
-            type="text"
-            value={input}
-            onChange={handleInputChange}
-            className="flex-grow p-4 text-base sm:mb-0 h-14 rounded-full caret-purple-600 text-gray-900"
-            placeholder="Ask Lexcribe"
-          />
-          <button
-            type="submit"
-            className="p-3 ml-2 text-gray-900 text-2xl bg-gradient-to-br from-teal to-sky-600 border border-gray-500 rounded-full w-14 h-14 transition duration-200"
-          >
-            <i className="fas fa-paper-plane"></i>
-          </button>
-        </form>
-        <p className="text-center mt-1 text-xs text-gray-400/70">Lexcribe may occasionally provide inaccurate information. Please verify any legal advice with authoritative sources.</p>
-      </motion.div>
-
-      {promptError && (
-        <motion.p
-          className="text-red-500 font-bold text-lg"
-          initial='hidden'
-          animate='visible'
-          variants={fadeVariants}
-        >
-          {promptError}
-        </motion.p>
-      )}
-    </section>
+            {promptError}
+          </motion.p>
+        )}
+      </section>
+    </AnimatePresence>
   );
 };
 
